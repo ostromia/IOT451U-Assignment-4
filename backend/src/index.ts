8820ef01337d82acdcd4392ee0a3075023b67354
ostromia <66202981+ostromia@users.noreply.github.com>
2025-01-07 19:55:06 +0000

    backend(feat): add requests for items
    various GET and POST requests to add, remove, and view all items in "inventory" table in database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,65 @@ const app = express();
 const port = 8080;
 app.use(express.json());
 
+// Open the database
+const db = new sqlite3.Database(`${__dirname}/database.db`, (error) => {
+    if (error) {
+        console.error(error.message);
+    }
+});
+
+app.get('/item', (request, response) => {
+    const query = "SELECT * FROM inventory";
+    db.all(query, [], (error, rows) => {
+        if (error) {
+            console.error(error.message);
+            response.status(500).json({ error: error.message });
+            return;
+        }
+        response.json(rows);
+    });
+});
+
+app.post('/add-item', (request, response) => {
+    const { name, url, image } = request.body;
+
+    if (!name || !url || !image) {
+        response.status(400).json({ error: "Missing required fields" });
+        return;
+    }
+
+    const query = 'INSERT INTO inventory (name, url, image) VALUES (?, ?, ?)';
+
+    db.run(query, [name, url, image], function (error) {
+        if (error) {
+            console.error(error.message);
+            response.status(500).json({ error: error.message });
+            return;
+        }
+
+        response.status(201).json({ message: "Item added successfully" });
+    });
+});
+
+app.post('/remove-item', (request, response) => {
+    const { id } = request.body;
+
+    const query = 'DELETE FROM inventory WHERE id = ?';
+
+    db.run(query, [id], function (error) {
+        if (error) {
+            console.error(error.message);
+            return response.status(500).json({ error: error.message });
+        }
+
+        if (this.changes === 0) {
+            return response.status(404).json({ error: "Item not found" });
+        }
+
+        response.status(201).json({ message: "Item removed successfully" });
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
